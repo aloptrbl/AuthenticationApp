29a84c2567130fd1d567242e13071c3449487a62
aloptrbl <thatzelo@gmail.com>
2022-01-26 05:26:39 +0800

    update page path 🅿️

--- app/src/main/java/io/zextech/authenticationapp/views/activities/SplashScreenActivity.kt
+++ app/src/main/java/io/zextech/authenticationapp/views/activities/SplashScreenActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.asLiveData
 import io.zextech.authenticationapp.R
 import io.zextech.authenticationapp.preferences.UserPreference
-import io.zextech.authenticationapp.views.fragments.HomeFragment
 
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -23,7 +22,7 @@ class SplashScreenActivity : AppCompatActivity() {
         Handler().postDelayed(Runnable {
             userPreferences.authtoken.asLiveData().observe(this, {
                 val activity =
-                    if (it == null) AuthActivity::class.java else HomeFragment::class.java
+                    if (it == null) AuthActivity::class.java else PageActivity::class.java
                 var intent = Intent(this, activity)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
